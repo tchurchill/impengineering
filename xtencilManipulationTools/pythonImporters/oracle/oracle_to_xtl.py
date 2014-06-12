2bf092754a9f0aea2bab8403fba7bef18264c8d2
Anthony Odufuye <toodufuye@spscommerce.com>
2014-06-12 15:00:24 -0500

    Added .gitignore file and more test data

--- xtencilManipulationTools/pythonImporters/oracle/oracle_to_xtl.py
+++ xtencilManipulationTools/pythonImporters/oracle/oracle_to_xtl.py
@@ -5,8 +5,6 @@ from subprocess import Popen, PIPE
 
 p = Popen([sys.executable, '856_importer.py'], stdin=PIPE, stdout=PIPE)
 result = json.loads(p.communicate(json.dumps(sys.argv))[0])
-with open('output.json', 'w') as output:
-    output.write(json.dumps(result))
 
-text = xtl('output.json')
+text = xtl(json.dumps(result))
 print text

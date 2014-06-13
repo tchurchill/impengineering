093ecb21b81bbc0d1e3fb4f5b6c6a684e663cdf1
Anthony Odufuye <odufuye.t@gmail.com>
2014-06-12 21:46:10 -0500

    json dumps and loads are broken

--- xtencilManipulationTools/pythonImporters/oracle/oracle_to_xtl.py
+++ xtencilManipulationTools/pythonImporters/oracle/oracle_to_xtl.py
@@ -10,22 +10,34 @@ parser.add_argument('infile', metavar = 'input', type = argparse.FileType('r'),
                     help = 'Input file to the Oracle Inporter')
 args = parser.parse_args()
 
-#Determine the type of Transaction and route the data to the appropriate script
+#Variables
+data_file = []
 oracle_document = False
+field_type = set(['VARCHAR2', 'DATE', 'NUMBER', 'CHAR', 'VARCHAR'])
+trans = {'INO' : '810', 'POI' : '850', 'POAO' : '855', 'DSNO' : '856'}
 
+#Determine the type of Transaction and route the data to the appropriate script
 for i in range(10):
     line = args.infile.readline().split()
     if len(line) > 4 and line[0] == 'Transaction' and line[3] == 'Report':
         oracle_document = True        
-    if len(line) > 5 and line[0] == 'Transaction:':
-        transaction_type = line[1]
 
 if not oracle_document:
     sys.exit()
 
+args.infile.seek(0)
+
+for lines in args.infile:
+    line = lines.split()
+    if len(line) > 5 and line[0] == 'Transaction:':
+        transaction_type = line[1]
+    if len(line) > 6 and line[5] in field_type:
+        data_file.append({line[1] : line})
+
+
 print transaction_type
 
-p = Popen([sys.executable, '856_importer.py'], stdin=PIPE, stdout=PIPE)
+p = Popen([sys.executable, trans[transaction_type] + '_importer.py'], stdin=PIPE, stdout=PIPE)
 result = json.loads(p.communicate(json.dumps(sys.argv))[0])
 
 text = xtl(json.dumps(result))
